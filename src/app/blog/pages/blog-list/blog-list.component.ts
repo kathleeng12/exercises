2a692a7ca510c786170b60fe5b516a298c3a5049
kathleeng <kmapsicay@77global.biz>
2022-09-29 00:50:01 +0800

    exercise3

--- src/app/blog/pages/blog-list/blog-list.component.ts
+++ src/app/blog/pages/blog-list/blog-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Blog } from '../../models/blog';
 import { BlogService } from '../../services/blog.service';
 
@@ -9,12 +10,19 @@ import { BlogService } from '../../services/blog.service';
 })
 export class BlogListComponent implements OnInit {
 
+
   blog?:Blog[] = [];
 
   constructor(private blogService:BlogService) { }
+  // constructor(private blogService:BlogService,
+  //   private route: ActivatedRoute , private router: Router) { }
 
   ngOnInit(): void {
     this.blog = this.blogService.getBlogDisplay();
   }
 
+
+  // navigate(){
+  //   this.router.navigate(['/blog']),{relativeTo:this.route}
+  // }
 }
